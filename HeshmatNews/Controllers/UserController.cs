02a33ad65cec54b3e5e80f2ad95ba6e4fd3a838a
DeV1 <danialshokouhmanesh@outlook.com>
2021-04-14 21:15:13 +0430

    downgrade to dotnet 3.1

---
+++ HeshmatNews/Controllers/UserController.cs
@@ -0,0 +1,58 @@
+using System.Linq;
+using System.Threading.Tasks;
+using dadachMovie.Services.Contracts;
+using HeshmastNews.Data;
+using HeshmastNews.DTOs.User;
+using HeshmastNews.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Internal;
+
+namespace HeshmastNews.Controllers
+{
+    [ApiController]
+    [Route("api/User")]
+    public class UserController : ControllerBase
+    {
+        private IUserService _userService;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private ApplicationDbContext _context;
+
+        public UserController(IUserService userService, SignInManager<IdentityUser> signInManager,
+            UserManager<IdentityUser> userManager, ApplicationDbContext context)
+        {
+            _userService = userService;
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _context = context;
+        }
+
+        [HttpPost("/Register")]
+        public User Post(UserRegisterViewModelDTO user)
+        {
+            return _userService.RegisterUser(user);
+        }
+
+        [HttpPost("/Login")]
+        [HttpPost("Login", Name = "Login")]
+        public async Task<ActionResult<UserTokenDTO>> Login([FromBody] UserLoginViewModelDTO model)
+        {
+            var result =
+                _context.Users.SingleOrDefault(u => u.Email == model.Email && u.PasswordHash == model.Password);
+            /*
+            var result =
+                _context.User.SingleOrDefault(u => u.Email == model.Email && u.Password == model.Password);
+                */
+
+            if (result != null)
+            {
+                return await _userService.BuildToken(model);
+            }
+            else
+            {
+                return BadRequest("Invalid login attempt");
+            }
+        }
+    }
+}
---
+++ HeshmatNews/Services/UserService.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper.Configuration;
+using dadachMovie.Services.Contracts;
+using HeshmastNews.Data;
+using HeshmastNews.DTOs.User;
+using HeshmastNews.Entities;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Extensions.Configuration;
+using IConfiguration = Microsoft.Extensions.Configuration.IConfiguration;
+
+namespace HeshmastNews.Services
+{
+    public class UserService : IUserService
+    {
+        private ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly IConfiguration _configuration;
+
+
+        public UserService(ApplicationDbContext context, UserManager<IdentityUser> userManager, IConfiguration configuration)
+        {
+            _context = context;
+            _userManager = userManager;
+            _configuration = configuration;
+        }
+
+        public User RegisterUser(UserRegisterViewModelDTO user)
+        {
+            var userVR = new User()
+            {
+                UserName = user.UserName,
+                Email = user.Email,
+                NameFamily = user.NameFamily,
+                Password = user.Password,
+                UserAvatar = "/User/UserProfile/default.png",
+                RegisterDate = DateTime.Now,
+                IsDelete = false
+            };
+
+            _context.User.Add(userVR);
+            _context.SaveChanges();
+            return userVR;
+        }
+
+        public async Task<UserTokenDTO> BuildToken(UserLoginViewModelDTO userInfo)
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.NameIdentifier, userInfo.Email),
+                new Claim(ClaimTypes.Name, userInfo.Email)
+            };
+            var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            var principal = new ClaimsPrincipal(identity);
+            
+        //    HttpContext.SignInAsync(principal);
+
+            var identityUser = await _userManager.FindByEmailAsync(userInfo.Email);
+         //   var claimsDB = await _userManager.GetClaimsAsync(identityUser);
+
+          //  claims.AddRange(claimsDB);
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["jwt:key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var expiration = DateTime.UtcNow.AddYears(1);
+
+            JwtSecurityToken token = new JwtSecurityToken(
+                issuer: null,
+                audience: null,
+                claims: claims,
+                expires: expiration,
+                signingCredentials: creds);
+
+            return new UserTokenDTO()
+            {
+                Token = new JwtSecurityTokenHandler().WriteToken(token),
+                Expiration = expiration
+            };
+        }
+    }
+}
